921428de2b8a50129cba4b63c4684ade2fe31641
Denis Lewandowski <denis.lewandowski@aptitudesoftware.com>
2021-12-05 19:56:05 +0100

    Initial Commit

---
+++ src/main/java/pl/denislewandowski/serverlogsreader/ServerLogsReaderApplication.java
@@ -0,0 +1,13 @@
+package pl.denislewandowski.serverlogsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerLogsReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerLogsReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/denislewandowski/serverlogsreader/ServerLogsReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.denislewandowski.serverlogsreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerLogsReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
